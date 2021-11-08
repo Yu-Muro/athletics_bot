a91b107b839c5d26a8a020b8990e8f96d9a7dcf3
Yu-Muro <6u38e7c16rnhg5e37a0z@gmail.com>
2021-11-09 02:32:43 +0900

    classを変更

--- main.py
+++ main.py
@@ -103,7 +103,7 @@ def send_message():
 def get_company_name(k):
     html = req.get(k)
     soup = BeautifulSoup(html.content, "html.parser")
-    temp = soup.select(".detailContent-box")[0].find('h2')
+    temp = soup.select(".columns is-marginless")[0]
     print(temp)
     #name = temp.text
     return None
